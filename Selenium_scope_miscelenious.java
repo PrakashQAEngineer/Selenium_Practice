ef5b55c11db717610ecb3a8360e98c5bbe59365b
Prakash Singh Rajput <35395076+PrakashQAEngineer@users.noreply.github.com>
2024-10-06 08:44:08 +0530

    Add files via upload

---
+++ Selenium_scope_miscelenious.java
@@ -0,0 +1,90 @@
+import java.time.Duration;
+import java.util.List;
+import java.util.Set;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium_scope_miscelenious 
+{
+	public static void main(String args[])
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		link(driver);
+		footerLink(driver);
+		footercol(driver);
+	}
+	
+		public static void link(WebDriver driver)
+		{
+			///to find the links on the whole page
+			
+			int li = driver.findElements(By.tagName("a")).size();
+			
+			System.out.println("size of the link: "+li);
+		}
+		
+		public static void footerLink(WebDriver driver)
+		{
+			//to find the link only on the footer
+			WebElement footer = driver.findElement(By.xpath("//div[@id = 'gf-BIG']"));
+			//Limiting the scope of driver from globally to the footer section only
+			
+			int foot_link = footer.findElements(By.tagName("a")).size();
+			System.out.println("The links on the footer are: "+foot_link);
+		}
+		
+		public static void footercol(WebDriver driver)
+		{
+			// to find the links for the first column only on the footer
+			WebElement footer = driver.findElement(By.xpath("//div[@id = 'gf-BIG']"));
+			WebElement foo_col = footer.findElement(By.xpath("//table[@class = 'gf-t'] //td[1]"));
+			 int foo_link = foo_col.findElements(By.tagName("a")).size();
+			 
+			 System.out.println("The links on the footer of first column are: "+foo_link);
+			
+			 //OPENING EVERY LINKS ON NEW TAB(Chord and sendkeys used for this)
+			 
+			 for(int i=0;i<foo_link;i++)
+			 {
+				 String newtab = Keys.chord(Keys.CONTROL,Keys.ENTER);
+				 foo_col.findElements(By.tagName("a")).get(i).sendKeys(newtab); 
+			 }
+			 
+			 //now move to the every tab and get the text of title of that website
+			 
+			 Set<String> wintab = driver.getWindowHandles();
+			 java.util.Iterator<String> it = wintab.iterator();
+			 String parent =it.next();// this is the main page
+			 driver.switchTo().window(parent); // we are in the main parent page
+			 
+			 String parent_title = driver.getTitle();
+			 System.out.println("The main parent page title is: "+parent_title);
+			 
+			 String child1 = it.next();
+			 driver.switchTo().window(child1);
+			 String child1_title = driver.getTitle();
+			 System.out.println(child1_title);
+			 
+			 String child2 = it.next();
+			 driver.switchTo().window(child2);
+			 String child2_title = driver.getTitle();
+			 System.out.println(child2_title);
+			 
+		}
+ 	
+	
+	
+	
+	
+
+}
