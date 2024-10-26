d2e4af6f651bb585c79cbb47fd4d68f35fe75b0a
Prakash Singh Rajput <35395076+PrakashQAEngineer@users.noreply.github.com>
2024-10-26 09:33:24 +0530

    Add files via upload

---
+++ Links_Count.java
@@ -0,0 +1,24 @@
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Links_Count 
+{
+	public static void main(String args[])
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
+		driver.get("https://www.flipkart.com/");
+          List<WebElement> wb1 = driver.findElements(By.tagName("a"));
+          System.out.println(wb1.size());
+		
+	}
+	
+
+}
