bf6f84d5785b55c971eaeba08de4ad52655ef15c
Prakash Singh Rajput <35395076+PrakashQAEngineer@users.noreply.github.com>
2025-04-26 08:49:59 +0530

    Add files via upload

---
+++ green_kart.java
@@ -0,0 +1,36 @@
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class green_kart 
+{
+	public static void main(String args[])
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+		List <WebElement> wb1 = driver.findElements(By.xpath("//h4[@class = 'product-name']"));
+		
+		//System.out.println(wb1.size());
+		for(int i=0;i<wb1.size();i++)
+		{
+			String veg = wb1.get(i).getText();
+			System.out.println(veg);
+			
+			String new_veg = veg.split("-")[0].trim();
+			System.out.println(new_veg);
+			
+			if(new_veg.equalsIgnoreCase("Potato"))
+			{
+				driver.findElements(By.xpath("//button[text() = 'ADD TO CART']")).get(i).click();
+			}
+		}
+	}
+
+}
