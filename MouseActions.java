7b70f10f879cf2b81d1c53f290e110e0a0fa70ea
Prakash Singh Rajput <35395076+PrakashQAEngineer@users.noreply.github.com>
2025-05-03 06:34:23 +0530

    Add files via upload

--- MouseActions.java
+++ MouseActions.java
@@ -3,6 +3,7 @@ package Actions;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -22,9 +23,26 @@ public class MouseActions
 		WebDriverWait w1 = new WebDriverWait(driver, Duration.ofSeconds(5));
 		w1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@id = 'nav-link-accountList-nav-line-1']")));
 		
+		//moving mouse to the Account and List section
 		WebElement wb1 = driver.findElement(By.xpath("//span[contains(@id,'nav-link-accountList')]"));
+		
+		//creating an object for the Action class
+		
 		Actions act = new Actions(driver);
 		act.moveToElement(wb1).build().perform();
+	   
+		WebDriverWait wt = new WebDriverWait(driver,Duration.ofSeconds(4));
+		wt.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id = 'twotabsearchtextbox']")));
+		
+		//move to the textbox and click on it
+		WebElement wb2 = driver.findElement(By.xpath("//input[@id = 'twotabsearchtextbox']"));
+		act.moveToElement(wb2).click().keyDown(Keys.SHIFT).sendKeys("toys").build().perform();
+		//act.moveToElement(wb1).build().perform();
+		act.moveToElement(wb2).doubleClick().build().perform();
+		act.moveToElement(wb1).contextClick().build().perform();
+		
+		
+		
 	}
 	
 
