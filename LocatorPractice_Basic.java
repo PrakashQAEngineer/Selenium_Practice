ec1a281a12d37a7cd1f46ca443f13a470fc8e9e9
Prakash Singh Rajput <35395076+PrakashQAEngineer@users.noreply.github.com>
2025-12-25 21:46:37 +0530

    LocatorPractice_Basic
    Updated user credentials and added password reset functionality.

--- LocatorPractice_Basic.java
+++ LocatorPractice_Basic.java
@@ -5,11 +5,12 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class LocatorPractice_Basic {
 	public static void main(String args[]) throws InterruptedException {
 		
-		String userName = "Prakash Singh Rajput";
+		String userName = "Jyoti Singh";
 		String pwd = "@QWertyuiop16";
 		
 		
@@ -47,8 +48,50 @@ public class LocatorPractice_Basic {
 		 Thread.sleep(3000);
 		 driver.findElement(By.cssSelector("input[type = 'text']:nth-child(2)")).clear();
 		 
-		 
+		 //entering the name
+		 driver.findElement(By.xpath("//form/input[1]")).sendKeys(userName); //parent child in xapth
+		 //entering the email
+		 driver.findElement(By.cssSelector("form input:nth-child(3)")).sendKeys("test");  ///parent child in css selector
          
+		 //click on the reset button
+		 driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+		 
+		 String getmssg = driver.findElement(By.tagName("p")).getText();
+		 System.out.println(getmssg);
+		 
+		 String finalPwd = getmssg.split("'")[1].split("'")[0].trim();
+		 System.out.println(finalPwd);
+		 
+		 driver.findElement(By.xpath("//button[text() = 'Go to Login']")).click();
+		 //Now Final Login
+		 
+		 Thread.sleep(3000);
+		 driver.findElement(By.cssSelector("input[id = 'inputUsername']")).sendKeys(userName);
+		 driver.findElement(By.cssSelector("input[name = 'inputPassword']")).sendKeys(finalPwd);
+		 
+		 //chkbox
+		 driver.findElement(By.xpath("//input[@id = 'chkboxOne']")).click();
+		 driver.findElement(By.xpath("//input[@id = 'chkboxTwo']")).click();
+		 
+		 driver.findElement(By.className("submit")).click();
+		 
+		 //after Login
+		 Thread.sleep(2000);
+		 		 
+		 String str = driver.findElement(By.xpath("//div[@class = 'login-container'] //h2")).getText();
+		 System.out.println(str);
+		 
+		 Assert.assertEquals(str, "Hello "+userName+",");
+		 String str2 = driver.findElement(By.xpath("//div[@class = 'login-container'] //h1")).getText();
+		 System.out.println(str2);
+		 
+		 String asst = driver.findElement(By.tagName("p")).getText();
+		 System.out.println(asst);
+		 
+		 Assert.assertEquals(asst, "You are successfully logged in.");
+		 
+		 //Logout Button
+		 driver.findElement(By.xpath("//button[text() = 'Log Out']")).click();
 	}
 
 }
