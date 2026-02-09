802354c0c2ede773a465dbfe8704a366b8580c9b
Prakash Singh Rajput <35395076+PrakashQAEngineer@users.noreply.github.com>
2026-02-09 06:50:32 +0530

    Add files via upload

---
+++ Wedding_Selenium/src/ScreenShots/Screenshot1.java
@@ -0,0 +1,33 @@
+package ScreenShots;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.v140.page.model.Screenshot;
+
+public class Screenshot1 
+{
+	public static void main(String args[]) throws IOException
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
+		driver.get("https://sparshyog.com/");
+		
+		File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		// Now you can do whatever you need to do with it, for example copy somewhere
+		FileUtils.copyFile(scrFile, new File("D:\\test\\screenshot.png"));
+		
+	}
+
+	
+	
+
+}
